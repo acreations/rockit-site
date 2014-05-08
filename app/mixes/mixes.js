c091e7a9a791c3c1515cbb79c2c34f56fdbb0035
acreations@aeris <aaron.wong.home@gmail.com>
2014-05-09 00:36:28 +0200

    Updated mixes when

--- app/mixes/mixes.js
+++ app/mixes/mixes.js
@@ -11,12 +11,79 @@ angular.module('mixes', [
   });
 })
 
-.controller('MixesCtrl', ['$scope', '$log', '$translatePartialLoader', '$translate', 'mixesRepository', 
-  function ($scope, $log, $translatePartialLoader, $translate, mixes) {
+.controller('MixesEditCtrl', function($scope, $modalInstance, editItem) {
+
+  $scope.edit = editItem;
+
+  $scope.ok = function () {
+
+    var hasError = false;
+
+    angular.forEach($scope.edit.actions, function(action) {
+      if(action.required && (!action.data || action.data.length == 0)) {
+        action.hasError = true;
+
+        hasError = true;
+      } 
+    });
+
+    if(!hasError) {
+      $modalInstance.close($scope.edit);
+    }
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+})
+
+.controller('MixesCtrl', ['$scope', '$log', '$translatePartialLoader', '$translate', '$modal', 'mixesRepository', 'repository',
+  function ($scope, $log, $translatePartialLoader, $translate, $modal, mixes, ds) {
+
+  $scope.holder = {
+    'when': [],
+    'then': [],
+    'finish': []
+  };
 
   $scope.selectWhen = function(item) {
     $log.debug('Selected when', item);
+
+    ds.get(item.url).then(
+      function(data) {
+        var modal = $modal.open({
+          templateUrl: 'mixes/mixes-when-edit.html',
+          controller: 'MixesEditCtrl',
+          resolve: {
+            editItem: function () {
+              var edit = {};
+
+              edit.item = item;
+              edit.actions = data.actions.POST; 
+
+              return edit;
+            }
+          }
+        });
+
+        modal.result.then(
+          function(edit) {
+            console.log("Add new when item to holder", edit);
+
+            $scope.holder.when.push(edit);
+          },
+          function() {
+            $log.debug("When modal dismissed ... not nothing");
+          }
+        );
+      },
+      function(error) {
+        console.log(error);
+      }
+    );
   } 
+
+
 
   var onCreate = function() {
     $translatePartialLoader.addPart('mixes');
@@ -42,3 +109,4 @@ angular.module('mixes', [
   onCreate();
 
 }]);
+
