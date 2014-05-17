4dd7c5ba0ca24a9a71a334d4f500db90c83b7338
acreations@aeris <aaron.wong.home@gmail.com>
2014-05-18 00:18:47 +0200

    Big update

--- app/mixes/mixes.js
+++ app/mixes/mixes.js
@@ -11,7 +11,7 @@ angular.module('mixes', [
   });
 })
 
-.controller('MixesEditCtrl', function($scope, $modalInstance, editItem) {
+.controller('MixesEditCtrl', function($scope, $modalInstance, $translate, editItem) {
 
   $scope.edit = editItem;
 
@@ -35,6 +35,32 @@ angular.module('mixes', [
   $scope.cancel = function () {
     $modalInstance.dismiss('cancel');
   };
+
+  var normalize = function(edit) {
+    angular.forEach(edit.actions, function(action) {
+      if(action.type === 'radio') {
+        var values = {};
+
+        angular.forEach(action.value, function(v) {
+          $translate(edit.item.modifier + '-radio-' + action.label + '-' + v).then(
+            function(translations) {
+              values[v] = translations;
+            },
+            function(error) {
+              console.log(error);
+            }
+          );
+        });
+        
+        action.data  = action.value[0];
+        action.value = values;
+      }
+    });
+
+    console.log(edit);
+  }
+
+  normalize($scope.edit);
 })
 
 .controller('MixesCtrl', ['$scope', '$log', '$translatePartialLoader', '$translate', '$modal', 'mixesRepository', 'repository',
@@ -72,18 +98,68 @@ angular.module('mixes', [
     );
   }
 
+  $scope.selectThen = function(item) {
+    $log.debug('Selected then', item);
+
+    select(item, 'then', 'mixes/mixes-when-edit.html', $scope.holder.then);
+  }
+
   $scope.selectWhen = function(item) {
     $log.debug('Selected when', item);
 
+    select(item, 'when', 'mixes/mixes-when-edit.html', $scope.holder.when);
+  } 
+
+  var normalize = function(container) {
+    if(container) {
+      var skipped = { 'razberry': '' };
+
+      angular.forEach(container, function(current) {
+        var skip = skipped.hasOwnProperty(current.association.entry);
+
+        angular.forEach(current.items, function(item) {
+          item.modifier = skip ? current.association.entry : item.identifier;
+        });
+      });
+    }
+  };
+
+  var onCreate = function() {
+    $translatePartialLoader.addPart('mixes');
+    $translate.refresh();
+
+    retrieveCapabilities();
+  };
+
+  var retrieveCapabilities = function() {
+    mixes.list().then(
+      function(response) {
+        $log.debug('Retrieved when statements', response);
+
+        $scope.when = response.when;
+        $scope.then = response.then;
+        $scope.finish = response.finish;
+
+        normalize($scope.when);
+        normalize($scope.then);
+        normalize($scope.finish);
+      },
+      function(error) {
+        $log.error('Could not get any when statements');
+      })
+  };
+
+  var select = function(item, type, templateUrl, holder) {
     ds.get(item.url).then(
       function(data) {
         var modal = $modal.open({
-          templateUrl: 'mixes/mixes-when-edit.html',
+          templateUrl: templateUrl,
           controller: 'MixesEditCtrl',
           resolve: {
             editItem: function () {
               var edit = {};
 
+              edit.type = type;
               edit.item = item;
               edit.actions = data.actions.POST; 
 
@@ -96,7 +172,7 @@ angular.module('mixes', [
           function(edit) {
             console.log("Add new when item to holder", edit);
 
-            $scope.holder.when.push(edit);
+            holder.push(edit);
           },
           function() {
             $log.debug("When modal dismissed ... not nothing");
@@ -107,46 +183,7 @@ angular.module('mixes', [
         console.log(error);
       }
     );
-  } 
-
-
-
-  var onCreate = function() {
-    $translatePartialLoader.addPart('mixes');
-    $translate.refresh();
-
-    retrieveCapabilities();
-  };
-
-  var set_icon_class_placeholder = function(container) {
-    var skipped = { 'razberry': '' };
-
-    angular.forEach(container, function(current) {
-      var skip = skipped.hasOwnProperty(current.association.entry);
-
-      angular.forEach(current.items, function(item) {
-        item.icon = skip ? current.association.entry : item.identifier;
-      });
-    });
   }
-
-  var retrieveCapabilities = function() {
-    mixes.list().then(
-      function(response) {
-        $log.debug('Retrieved when statements', response);
-
-        $scope.when = response.when;
-        $scope.then = response.then;
-        $scope.finish = response.finish;
-
-        set_icon_class_placeholder($scope.when);
-        set_icon_class_placeholder($scope.then);
-        set_icon_class_placeholder($scope.finish);
-      },
-      function(error) {
-        $log.error('Could not get any when statements');
-      })
-  };
 
   onCreate();
 
